103d9637eb3abdded48a991e94326852b6c8f1e9
Ayesha Qahash <138151892+alq5630@users.noreply.github.com>
2023-09-19 15:49:08 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import openai
+import gradio as gr
+
+openai.api_key = "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"
+
+messages = [
+    {"role": "system", "content": "You are a helpful and kind AI Assistant."},
+]
+
+def chatbot(input):
+    if input:
+        messages.append({"role": "user", "content": input})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+inputs = gr.inputs.Textbox(lines=7, label="Chat with AI")
+outputs = gr.outputs.Textbox(label="Reply")
+
+gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="AI Chatbot",
+             description="Ask anything you want",
+             theme="compact").launch(share=True)
