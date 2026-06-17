e6e71f3e5453b3329a8172f83150dc7b302a8068
Ayesha Qahash <138151892+alq5630@users.noreply.github.com>
2026-06-17 17:24:33 -0400

    Update iframe source for chapter 2 content

--- script.js
+++ script.js
@@ -418,7 +418,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       document.getElementById('chapter2Link').addEventListener('click', function(e) {
         e.preventDefault();
-        loadMainContent('<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTn841Ne95gmqNAld0sUyIlxDel0rCmRxAQUzUwIhN8d6DYdfPWeQwlcdLKS9GegQ/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>');
+        loadMainContent('<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTn841Ne95gmqNAld0sUyIlxDel0rCmRxAQUzUwIhN8d6DYdfPWeQwlcdLKS9GegQ/pubembed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="749" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>');
       });
 
       document.getElementById('chapter1Link').addEventListener('click', function(e) {
